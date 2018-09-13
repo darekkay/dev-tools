f044a5d4f39c3f10b1afd04d848ea2da5ab0ee6c
Darek Kay <darekkay@gmail.com>
2018-09-13 13:31:54 +0200

    Init repository

---
+++ README.md
@@ -0,0 +1,29 @@
+# Developer Tools
+
+A collection of small tools and utilities for software developers.
+
+## Flexbox Cheatsheet
+
+A graphical overview of the [CSS Flexible Box Layout](https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout) properties. It is optimized to be printed on a single landscape format paper.
+
+Based on [CSS Cheats](https://github.com/sakamies/css-cheats) by @sakamies.
+
+ - [Live Demo](https://darekkay.com/dev/flexbox-cheatsheet.html)
+
+![Flexbox Cheatsheet](src/assets/img/preview-flexbox-cheatsheet.png)
+
+## Color Tools
+
+A simple color converter and visualizer.
+
+Inspired by [Contrast Ratio](https://contrast-ratio.com/) and [Colorrrs](https://www.webpagefx.com/web-design/hex-to-rgb/).
+
+ - [Live Demo](https://darekkay.com/dev/color-tools.html)
+
+![Color Tools](src/assets/img/preview-color-tools.png)
+
+## License
+
+Copyright 2018 Darek Kay <hello@darekkay.com>  
+
+This project and its contents are open source under the [MIT license](LICENSE).
---
+++ src/color-tools/colors.js
@@ -0,0 +1,146 @@
+const selectElements = name => {
+  const display = document.getElementById(`display-${name}`);
+  return {
+    display,
+    input: document.getElementById(`input-${name}`),
+    picker: document.getElementById(`picker-${name}`),
+    examples: Array.from(document.getElementsByClassName(`example-${name}`)),
+    output: {
+      hex: display.getElementsByClassName("output-hex")[0],
+      rgb: display.getElementsByClassName("output-rgb")[0]
+    }
+  };
+};
+
+const alpha = selectElements("alpha");
+const beta = selectElements("beta");
+const contrastRatio = document.getElementById("contrast-ratio");
+
+const defaultAlphaColor = one.color("#dea");
+const defaultBetaColor = one.color("#222");
+
+// create favicon canvas
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 16;
+canvas.height = 16;
+document.body.appendChild(canvas);
+
+const normalizeValue = value => (value ? value.trim() : value);
+
+const getColor = value => one.color(normalizeValue(value));
+
+const toggleClass = (element, conditions) => {
+  Object.entries(conditions).forEach(
+    ([key, value]) =>
+      value ? element.classList.add(key) : element.classList.remove(key)
+  );
+};
+
+const updateContrastRatio = () => {
+  const alphaColor = getColor(alpha.input.value) || defaultAlphaColor;
+  const betaColor = getColor(beta.input.value) || defaultBetaColor;
+
+  // update contrast ratio
+  const contrast = alphaColor.contrast(betaColor);
+  contrastRatio.innerText = Math.round(contrast * 100) / 100;
+
+  toggleClass(contrastRatio, {
+    "wcag-aaa": contrast >= 7.1,
+    "wcag-aa": contrast >= 4.5,
+    "wcag-fail": contrast < 4.5
+  });
+};
+
+const updateFavicon = () => {
+  const alphaColor = getColor(alpha.input.value) || defaultAlphaColor;
+  const betaColor = getColor(beta.input.value) || defaultBetaColor;
+
+  ctx.clearRect(0, 0, 16, 16);
+
+  ctx.fillStyle = alphaColor.hex();
+  ctx.fillRect(0, 0, 8, 16);
+
+  ctx.fillStyle = betaColor.hex();
+  ctx.fillRect(8, 0, 8, 16);
+
+  document.getElementById("favicon").setAttribute("href", canvas.toDataURL());
+};
+
+/* eslint-disable no-param-reassign */
+const updateColor = (panel, otherPanel, color) => {
+  panel.display.style.background = color.cssa();
+  otherPanel.display.style.color = color.cssa();
+
+  panel.output.rgb.innerText = color.alpha() === 1 ? color.css() : color.cssa();
+  panel.output.hex.innerText = color.hex();
+
+  panel.picker.value = color.hex();
+
+  toggleClass(panel.display, {
+    dark: color.isDark(),
+    light: color.isLight()
+  });
+
+  if (alpha.input.value && beta.input.value) {
+    // save state to url hash
+    window.location.hash = encodeURIComponent(
+      `${alpha.input.value}-and-${beta.input.value}`
+    );
+  }
+
+  updateContrastRatio();
+  updateFavicon();
+};
+
+const setInputValue = (panel, value) => {
+  panel.input.value = value;
+  panel.input.dispatchEvent(new Event("input"));
+};
+
+const preparePanels = (panel, otherPanel) => {
+  panel.input.addEventListener("input", event => {
+    const color = getColor(event.target.value);
+    if (color) {
+      updateColor(panel, otherPanel, color);
+    }
+  });
+
+  panel.picker.addEventListener("input", event => {
+    const color = getColor(event.target.value);
+    if (color) {
+      setInputValue(panel, event.target.value);
+    }
+  });
+
+  panel.input.addEventListener("keyup", event => {
+    // clear input on Escape
+    const key = event.key || event.keyCode;
+    if (key === "Escape" || key === "Esc" || key === 27) {
+      panel.input.value = "";
+    }
+  });
+
+  panel.examples.forEach(example => {
+    example.addEventListener("click", () => {
+      setInputValue(panel, example.innerText);
+    });
+  });
+};
+
+preparePanels(alpha, beta);
+preparePanels(beta, alpha);
+
+updateColor(alpha, beta, defaultAlphaColor);
+updateColor(beta, alpha, defaultBetaColor);
+
+const { hash } = window.location;
+if (hash) {
+  // load state from URL hash
+  const state = decodeURIComponent(hash.substring(1)).split("-and-");
+  if (state.length === 2) {
+    setInputValue(alpha, state[0]);
+    setInputValue(beta, state[1]);
+  }
+}
